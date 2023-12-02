9f28f2841ec113442012d1254b9892db5086def7
Dev-olimjon <olimjonmakhmudov26156@gmail.com>
2023-12-02 23:06:03 +0500

    first commit

--- src/history/history.service.ts
+++ src/history/history.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import {InjectModel} from "@nestjs/sequelize";
 import {HistoryTable} from "./madel/history.madel";
+import { HistoryDto } from './dto/history.dto';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class HistoryService {
     constructor(@InjectModel(HistoryTable) private historyRepository: typeof HistoryTable) { }
 
+    async create(history: HistoryDto) {
+        let newHistory: HistoryDto = {
+          id: uuid(),
+          userId: history.userId,
+          bookId: history.bookId  
+        }
+        return await this.historyRepository.create(newHistory)
+    }
 
+    async GetAll() {
+        
+    }
 }
