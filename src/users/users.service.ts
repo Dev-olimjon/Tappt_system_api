c885c95179a9e6d1edff8e3f5902208c7a4fb368
Max-Vpire <105534593+Max-Vpire@users.noreply.github.com>
2025-01-04 01:24:10 +0500

    Update users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,6 +14,7 @@ import { sql } from '@sequelize/core';
 export class UsersService {
     constructor(@InjectModel(UsersTable) private userRepository: typeof UsersTable) {}
 
+
     async AddUser(User: RegisterDto) {
       const hashedPassword = await bcrypt.hash(User.password, 10)
       let CreatingUser: UserDto = {
